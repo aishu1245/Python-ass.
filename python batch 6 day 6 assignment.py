8063b836f92da29a218a4ad580e454ee7621d94e
aishu1245 <68596147+aishu1245@users.noreply.github.com>
2020-07-28 10:58:41 +0530

    Add files via upload

---
+++ python batch 6 day 6 assignment.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment 6
+# 
+# -list comprehnsion 
+
+# In[2]:
+
+
+list1 = [1,2,3,4,5,7,8]
+list2 = ['a','b','c','d','e']
+dict1 = {list1[each]:list2[each] for each in range(min(len(list1),len(list2)))}
+print(dict1)
+
+
+#  i have submitted all the assignment in githum but not uploded on the link again on assignment submision link , so today i've uploaded the link of that folder where you can see the date of each assignment i ve submitted please check it , sorry for inconvinience . 
